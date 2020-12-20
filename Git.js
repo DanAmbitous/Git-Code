6b86b610cd9326600eaa88a01bda88fc73c48ae3
Dan <danazad@gmail.com>
2020-12-20 17:23:44 -0500

    Added a button and info

--- Git.js
+++ Git.js
@@ -1,3 +1,12 @@
 $(function() {
     $('form').prepend('The form is still being worked');
-})
+});
+
+$(function() {
+    let form = $('<h5></h5>').text('For more information, you can send questions here');
+    $('form').after(form)
+});
+
+$(function() {
+    $(console.log('This works'));
+});
