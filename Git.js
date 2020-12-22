ecb7d9383b69bfadbd7b38a9f73a2ea85aaebb61
Dan <danazad@gmail.com>
2020-12-21 21:55:27 -0500

    Updated all of the web files

--- Git.js
+++ Git.js
@@ -1,5 +1,30 @@
-$(function() {
-    $('button').click(function() {
-        $('button').after(prompt());
-    })
-})
+$('#thePages').hide();
+//$('#mainPage').hide();
+$('#utilizationsPage').hide();
+$('#bioPage').hide();
+
+// The beginning of the links in the navigation bar
+/*
+$('#main').click(function() {
+    $('#mainPage').show();
+    $('#thePages').show();
+    $('#utilizationsPage').hide();
+    $('#bioPage').hide();
+
+});
+
+$('#utilizations').click(function() {
+    $('#utilizationsPage').show();
+    $('#thePages').show();
+    $('#mainPage').hide();
+    $('#bioPage').hide();
+});
+
+$('#bio').click(function() {
+    $('#bioPage').show();
+    $('#thePages').show();
+    $('#mainPage').hide();
+    $('#utilizationsPage').hide();
+});
+
+// This is the end of the links in the navigation bar
