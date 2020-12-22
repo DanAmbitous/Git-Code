e2af7a3e716cc3ab539d905e9c0e8f5388150566
Dan <danazad@gmail.com>
2020-12-21 23:35:27 -0500

    Added new content in each tab

--- Git.js
+++ Git.js
@@ -1,16 +1,19 @@
-$('#thePages').hide();
-//$('#mainPage').hide();
+$('#thePages').show();
+$('#mainPage').hide();
 $('#utilizationsPage').hide();
+$('#overviewPage').show();
+$('#questionsAndAnswersPage').hide();
 $('#bioPage').hide();
 
 // The beginning of the links in the navigation bar
-/*
+
 $('#main').click(function() {
     $('#mainPage').show();
     $('#thePages').show();
     $('#utilizationsPage').hide();
     $('#bioPage').hide();
-
+    $('#overviewPage').hide();
+    $('#questionsAndAnswersPage').hide();
 });
 
 $('#utilizations').click(function() {
@@ -18,13 +21,33 @@ $('#utilizations').click(function() {
     $('#thePages').show();
     $('#mainPage').hide();
     $('#bioPage').hide();
+    $('#overviewPage').hide();
+    $('#questionsAndAnswersPage').hide();
 });
+
+$('#overview').click(function() {
+    $('#overviewPage').show();
+    $('#thePages').show();
+    $('#mainPage').hide();
+    $('#utilizationsPage').hide();
+    $('#questionsAndAnswersPage').hide();
+})
+
+$('#questionsAndAnswers').click(function() {
+    $('#questionsAndAnswersPage').show();
+    $('#thePages').show();
+    $('#bioPage').hide();
+    $('#mainPage').hide();
+    $('#utilizationsPage').hide();
+    $('#overviewPage').hide();
+})
 
 $('#bio').click(function() {
     $('#bioPage').show();
     $('#thePages').show();
     $('#mainPage').hide();
     $('#utilizationsPage').hide();
+    $('#questionsAndAnswersPage').hide();
 });
 
 // This is the end of the links in the navigation bar
